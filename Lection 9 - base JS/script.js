0fbe0775b1ff028af099974fd3389d52c0c5219a
zhivs <zhivsy@mail.ru>
2025-09-10 23:29:38 +0300

    Лекции 7-9

---
+++ Lection 9 - base JS/script.js
@@ -0,0 +1,99 @@
+
+//Числа в JS
+
+let delay_mcs = 1e-6
+
+let_delay_mcs1 = 0.000001
+
+//console.log(delay_mcs)
+
+//console.log(let_delay_mcs1)
+
+
+// Строки
+
+let name = "Ivan"
+
+//Кавычки
+
+let single = 'singe_qouted'
+
+let duble = "double_qouted"
+
+let backqoute = `backqoute`
+
+function sum(a, b) {
+    return a + b
+}
+
+//console.log(`2+2 =${sum(2,2)}`)
+
+let a = `addsa
+asdasd`
+
+//console.log(a)
+
+// Спецсимволы
+
+// Перенос строки (перевод строки) \n
+// Знак табуляции \t
+// Возвращение каретки \r 
+
+//Доступ к символам
+
+let myname = "Ivan"
+
+//console.log(myname[1])
+
+let myunderfinedvar;
+
+//typeof
+//console.log(typeof myname)
+
+
+//alert('Привет, закрой меня!')
+
+//Массивы
+
+let fruits = ["Яблоко", "Апельсин", "Груша"]
+
+
+//console.log(fruits[10])
+
+
+fruits[2] = 'Cлива'
+
+//console.log(fruits)
+
+fruits[3] = 'Груша'
+
+//console.log(5+2)
+
+//console.log(5-2)
+
+//console.log(5*2)
+
+//console.log(5/2)
+
+//console.log(5 % 2)
+
+console.log(5 == 34)
+
+console.log(5 > 34)
+
+console.log(5 < 34)
+
+
+console.log(5 != 34)
+
+console.log(5 == '5')
+
+console.log(5 === '5')
+
+console.log(true == 1)
+
+https://disk.yandex.ru/i/PRsI7keakKZ9dQ
+
+https://disk.yandex.ru/i/wM7NG0tF4EtUkg
+
+https://disk.yandex.ru/i/O_hG56uCzl_fiQ
