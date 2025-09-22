34510b2bdbba14a277d36f7cbd3fbc88fa7f6c37
zhivs <zhivsy@mail.ru>
2025-09-22 22:16:29 +0300

    Добавление файла для отладки python

--- Lection 15 - Python/main.py
+++ Lection 15 - Python/main.py
@@ -1,8 +1,13 @@
-print('hello, world')
+# main.py
+def calculate_sum(a, b):
+    result = a + b  # ← сюда поставим breakpoint
+    return result
 
-a = 5
+def main():
+    x = 5
+    y = 7
+    total = calculate_sum(x, y)
+    print(f"Сумма {x} и {y} = {total}")
 
-b = 123
-
-
-print(a + b)
+if __name__ == "__main__":
+    main()
